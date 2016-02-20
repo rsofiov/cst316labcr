98a9049ce9da15bfe20da18d3ece5da98fd8a9e8
rsofiov <emilybyrne@emilys-MacBook-Pro.local>
2016-02-20 15:12:03 -0700

    Fixed issue #6

--- src/banking/primitive/core/Checking.java
+++ src/banking/primitive/core/Checking.java
@@ -2,7 +2,8 @@ package banking.primitive.core;
 
 public class Checking extends Account {
 
-	private static final long serialVersionUID = 11L;
+	//private static final long SERIAL_VERSION_ID serialVersionUID = 11L;
+	private static final long SERIAL_VERSION_ID  = 11L;
 	private int numWithdraws = 0;
 	
 	private Checking(String name) {
