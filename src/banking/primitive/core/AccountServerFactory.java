46c15a1410fa49446e5238e7deedaf53a45beebc
sgarci33 <sgarci33@asu.edu>
2016-02-22 00:45:14 -0700

    Trying to fix issue #2 again

--- src/banking/primitive/core/AccountServerFactory.java
+++ src/banking/primitive/core/AccountServerFactory.java
@@ -8,7 +8,15 @@ public class AccountServerFactory {
 	protected AccountServerFactory() {
 
 	}
-
+	/**
+	  Method: getMe()
+	  Inputs: n/a
+	  Returns: singleton - an AccountServerFactory object
+	​
+	  Description: Returns the singleton AccountServerFactory;
+	  if there is no such thing, a new one is created in its
+	  place. 
+	*/
 	public static AccountServerFactory getMe() {
 		if (singleton == null) {
 			singleton = new AccountServerFactory();
@@ -16,7 +24,13 @@ public class AccountServerFactory {
 
 		return singleton;
 	}
-
+	/**
+	  Method: lookup()
+	  Inputs: n/a
+	  Returns: an account server
+	​
+	  Description: Looks up and searches for an account server.
+	*/
 	public AccountServer lookup() {
 		return new ServerSolution();
 	}
