b2bd6b7ddf43caacee70bbfd62ac37a6c9aeeacf
sgarci33 <sgarci33@asu.edu>
2016-02-22 20:06:54 -0700

    Fixed Issue #30. (Please read lines 25-28 in Main.java for further elaboration)

--- src/banking/gui/Main.java
+++ src/banking/gui/Main.java
@@ -21,6 +21,12 @@ final class Main {
 	 */
 	public static void main(final String[] args) throws Exception {
 
+		/*
+		 * Make sure the command-line argument in Eclipse is set
+		 * to "my.properties". To do so, go to Run Configurations ->
+		 * Variables, and insert "my.properties" as one of the
+		 * program variables.
+		 */
 		if (args.length != 1) {
 			System.out.println("Usage: java FormMain <property file>");
 			System.exit(1);
